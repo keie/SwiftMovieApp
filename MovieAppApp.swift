4700a0450e73da6f7e9e3dc0ed770878fd32a34f
kevin.nava@externos-cl.cencosud.cl <appleuser@Apples-MacBook-Pro.local>
2022-11-05 14:45:46 -0400

    initial commit

---
+++ MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Apple User on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
